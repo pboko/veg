69b125d97567b429c5add551100aea42389a2e8f
lindavte <linda.tahraoui@voseconomies.fr>
2015-07-22 18:45:20 +0200

    Ajout de nouveaux champs dans la table User

--- src/VTE/VEGBundle/Entity/User.php
+++ src/VTE/VEGBundle/Entity/User.php
@@ -18,10 +18,194 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @var string
+     * @ORM\Column(name="last_name", type="string", nullable=false)
+     */
+    private $last_name;
+
+    /**
+     * @var string
+     * @ORM\Column(name="first_name", type="string", nullable=false)
+     */
+    private $first_name;
+    
+    /**
+     * @var string
+     * @ORM\Column(name="telephone", type="string", nullable=true)
+     */
+    private $telephone;
+    
+    /**
+     * @var string
+     * @ORM\Column(name="address", type="string", nullable=true)
+     */
+    private $address;
+    
+    /**
+     * @var string
+     * @ORM\Column(name="zipcode", type="string", nullable=true)
+     */
+    private $zipcode;
+    
+    /**
+     * @var string
+     * @ORM\Column(name="city", type="string", nullable=true)
+     */
+    private $city;
 
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set last_name
+     *
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->last_name = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get last_name
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * Set first_name
+     *
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->first_name = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get first_name
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * Set telephone
+     *
+     * @param string $telephone
+     * @return User
+     */
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    /**
+     * Get telephone
+     *
+     * @return string 
+     */
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return User
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set zipcode
+     *
+     * @param string $zipcode
+     * @return User
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    /**
+     * Get zipcode
+     *
+     * @return string 
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
     }
 }
