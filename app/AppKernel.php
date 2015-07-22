18642d7c21956aadc99b64aab01983ab0cf5656c
lindavte <linda.tahraoui@voseconomies.fr>
2015-07-22 17:18:46 +0200

    Installer FOSUserBundle et creation d'un class user
    1. installation de FOSUserBundle
    2. activer FOS/UserBundle
    3. Creation de la class User dans VEGBundle
    4. Configuration et Sécurité
    5. Migration de la base de donnée

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new VTE\VEGBundle\VTEVEGBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
