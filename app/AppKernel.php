bc4ea7046ab26b95e405b755ea94e29c3ddfadc7
lindavte <linda.tahraoui@voseconomies.fr>
2015-07-24 11:42:36 +0200

    Installation de sonataAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,14 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new VTE\VEGBundle\VTEVEGBundle(),
           
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+          
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            
+            new Sonata\AdminBundle\SonataAdminBundle(),
+          
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
